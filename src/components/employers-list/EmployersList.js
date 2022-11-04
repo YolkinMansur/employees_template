1210d37ea005e62170063a1f3cd8005a164a2639
YolkinMansur <yolkinmansur@gmail.com>
2022-11-04 14:38:03 +0600

    first commit

---
+++ src/components/employers-list/EmployersList.js
@@ -0,0 +1,23 @@
+import EmployersListItem from '../employers-list-item/EmployersListItem';
+
+import './EmployersList.css';
+
+const EmployersList = ({ data, onDelete, onToggleProp }) => {
+  const elements = data.map(item => {
+    const { id, ...itemProps } = item;
+    return (
+      <EmployersListItem
+        key={id}
+        {...itemProps}
+        onDelete={() => onDelete(id)}
+        onToggleProp={e =>
+          onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))
+        }
+      />
+    );
+  });
+
+  return <ul className="app-list list-group">{elements}</ul>;
+};
+
+export default EmployersList;
